ef210349af02574e561effd4bdc6296eb750807d
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-01-27 21:42:06 +0000

    feat: make html

--- src/build.js
+++ src/build.js
@@ -4,14 +4,12 @@ import { join } from 'path';
 import { parse } from './parser.js';
 import { read_txt } from './read_data.js';
 import { calculateAnalysis } from './calc.js'
-import { makeIndex } from './make_html.js';
+import { entryTemplate, makeIndex, makeHTML } from './make_html.js';
 
 
 
 const DATA_DIR = './data';
 const OUTPUT_DIR = './dist';
-const filename = './data/3.txt'
-
 /**
  * Checks if a directory exists
  * @param {*} dir Directory to check
@@ -27,34 +25,49 @@ async function direxists(dir) {
 }
 
 async function main() {
-  // const files = await readdir(DATA_DIR);
+  const files = await readdir(DATA_DIR);
 
   if (!(await direxists(OUTPUT_DIR))) {
     await mkdir(OUTPUT_DIR);
   }
-  const index = makeIndex();
+
+  const numAnlys = [];
+
+  for (const file of files) {
+    const path = join(DATA_DIR, file);
+    const info = await stat(path);
+
+    if (info.isDirectory()) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+    const str = await read_txt(path);
+    const parsed = await parse(str);
+    const calced = await calculateAnalysis(parsed)
+    const slug = file.split('.')[0];
+    const filename = join(OUTPUT_DIR, `dataset_${slug}.html`);
+
+    // html stuff
+    const html = makeHTML(calced)
+    const title = 'Dataset '.concat(slug)
+    const numEntry = entryTemplate(title, html, true)
+
+    if (filename) {
+      await writeFile(filename, numEntry, { flag: 'w+' });
+      numAnlys.push(slug);
+    } else {
+      console.warn('Faulty filename', path);
+    }
+  }
+  const index = entryTemplate('Gagnavinnsla', makeIndex(numAnlys));
   await writeFile(join(OUTPUT_DIR, 'index.html'), index, { flag: 'w+' });
-
-
-  // const numAnlys = [];
-
-  // for (const file of files) {
-  //   const path = join(DATA_DIR, file);
-  //   const info = await stat(path);
-
-  //   if (info.isDirectory()) {
-  //     // eslint-disable-next-line no-continue
-  //     continue;
-  //   }
-
-
-  // }
   // console.log(file.split('.')[0])
 
   // const a = await read_txt('./data/1.txt');
   // const b = await parse(a);
   // const c = await calculateAnalysis(b);
   // console.log(c)
+
 
 }
 
--- src/make_html.js
+++ src/make_html.js
@@ -2,29 +2,35 @@ export function makeHTML(entry, dataTitle) {
   const template = `
     <section>
       <h1>Data ${dataTitle}</h1>
-      ${entry}
+      <ul>
+        <li><strong>Variance: </strong>${entry.variance}</li>
+        <li><strong>Max: </strong>${entry.max}</li>
+        <li><strong>Mean: </strong>${entry.mean}</li>
+        <li><strong>Median: </strong>${entry.median}</li>
+        <li><strong>Min: </strong>${entry.min}</li>
+        <li><strong>Standard deviation: </strong>${entry.stddev}</li>
+        <li><strong>Sum: </strong>${entry.sum}</li>
+        <li><strong>Range: </strong>${entry.range}</li>
+      </ul>
     </section>
   `;
   return template;
 
 }
 
-export function makeIndex() {
-  return `<!DOCTYPE html >
-    <html>
-      <head>
-        <title>Gagnavinnsla</title>
-      </head>
-      <body>
-        <p>Here we have some text <strong>BOLD</strong> and it should show up</p>
-      </body>
-    </html>`;
+export function makeIndex(entries) {
+  let list = '';
+  for (const entry of entries) {
+    const link = `<li><a href="${`dataset_${entry}.html`}">Dataset ${entry}</a></li>`;
+    list += link;
+  }
+  return `<ul>${list}</ul>`;
 }
 
 /**
- * Takes HTML for a single blog entry and returns it with the site template.
+ * Takes HTML for a single entry and returns it with the site template.
  */
-export function blogTemplate(title, content, showBack = false) {
+export function entryTemplate(title, content, showBack = false) {
   const back = showBack ? '<p><a href="/">Til baka</a></p>' : '';
   return `
   <!doctype html>
